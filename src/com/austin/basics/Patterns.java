cb2102ce6df1f8bfe6b9c265b752fffbbc2954c9
Austin Johny <austinjohny9868@gmail.com>
2022-08-25 20:11:44 +0530

    Pattern to print star of length n

--- src/com/austin/basics/Patterns.java
+++ src/com/austin/basics/Patterns.java
@@ -16,9 +16,37 @@ public class Patterns {
             System.out.println();
         }
     }
+    public static void star(int n){
+        if(n%2 == 0){
+            System.out.println("Can't be a star");
+            return;
+        }
+        for(int i = 0; i < n;i++){
+            for(int j = 0;j<n;j++){
+                if(i <= n/2){
+                    if(j >= n/2 -i && j <= n/2 +i){
+                        System.out.print("#");
+                    }
+                    else{
+                        System.out.print(" ");
+                    }
+                }
+                else{
+                    if(j >= (n/2 - (n-1-i)) && j <= (n/2 + (n-1-i))){
+                        System.out.print("#");
+                    }
+                    else{
+                        System.out.print(" ");
+                    }
+                }
+            }
+            System.out.println();
+        }
+    }
 
     public static void main(String[] args) {
        Scanner sc = new Scanner(System.in);
-       stairCase(4);
+//       stairCase(4);
+        star(11);
     }
 }
