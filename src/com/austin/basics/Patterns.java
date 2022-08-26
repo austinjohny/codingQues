9ddc2fd95f92d1c0d00aeb6d5445a46115171903
Austin Johny <austinjohny9868@gmail.com>
2022-08-26 19:26:25 +0530

    Right triangle star pattern

--- src/com/austin/basics/Patterns.java
+++ src/com/austin/basics/Patterns.java
@@ -3,6 +3,15 @@ import java.util.*;
 
 public class Patterns {
 
+    public static void rightTriangleStar(int n){
+        for(int i = 0;i < n;i++){
+            for(int j = 0;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
     public static void stairCase(int n){
         for(int i = 0;i<n;i++){
             for(int j = 0;j<n;j++){
@@ -47,6 +56,7 @@ public class Patterns {
     public static void main(String[] args) {
        Scanner sc = new Scanner(System.in);
 //       stairCase(4);
-        star(11);
+//        star(11);
+        rightTriangleStar(5);
     }
 }
